fac2c9e2cb4009942ffaf2dc01f2de61c683693f
Tammam <tammam140a@gmail.com>
2024-09-18 17:32:07 +0300

    first commit

---
+++ src/main/java/UniversityClubs/Clubs/ClubsApplication.java
@@ -0,0 +1,13 @@
+package UniversityClubs.Clubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubsApplication.class, args);
+	}
+
+}
